5f34fcc968c68939aeeede546b6711ed65caaa07
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-23 09:07:10 +0200

    Ejercicio de la presentación

---
+++ Tema 1/script.js
@@ -0,0 +1,10 @@
+function init () {
+
+  document.getElementById ("pulsador").addEventListener("click", function () {
+    cambiarTexto();
+  });
+}
+
+function cambiarTexto() {
+  document.getElementById ("texto").innerHTML = "<p style='color:black'><h1>¡Hola mundo!</h1></p>"
+}
