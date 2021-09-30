f157c39eb4928c95ebed3e3f06ffbc1c14e85c05
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-30 09:12:25 +0200

    Añadir nuevos métodos y optimizar el código

--- Tema 2/Apartado 4 y 5/script.js
+++ Tema 2/Apartado 4 y 5/script.js
@@ -1,6 +1,48 @@
+// I/O alert, console.log, confirm y prompt
 alert("Hola mundo");
 
 console.log("Otro hola mundo");
 
 let respuesta = confirm ("¿Desea cancelar la suscripción?");
 alert ("Usted ha contestado que " + respuesta);
+
+// Operadores aritméticos
+function operadoresAritmeticos(num1, num2) {
+  /* let num1 = 3;
+  let num2 = 5; */
+  console.log (num1 + " + " + num2 + " = " + (num2 += num1)); // 8
+  console.log (num1 + " - " + num2 + " = " + (num2 -= num1)); // 2
+  console.log (num1 + " * " + num2 + " = " + (num2 *= num1)); // 15
+  console.log (num1 + " / " + num2 + " = " + (num2 /= num1)); // 1
+  console.log (num1 + " % " + num2 + " = " + (num2 %= num1)); // 2
+}
+
+// Asignación
+let num1 = 5;
+let num2 = 8;
+let resultado1;
+let resultado2;
+
+resultado1 = ((num1+num2) * 200) / 100;
+resultado2 = resultado1 % 3;
+resultado1 = ++num1;
+resultado2 = num2++;
+resultado1 = --num1;
+resultado2 = num2--;
+resultado1 = -resultado2;
+
+// Comparación
+let a = 4;
+let b = 5;
+let c = "5";
+
+console.log("El resultado de la expresión: " + (a == c));
+console.log("El resultado de la expresión: " + (a === c));
+console.log("El resultado de la expresión: " + (a != c));
+console.log("El resultado de la expresión: " + (a !== c));
+console.log("El resultado de la expresión: " + (a == b));
+console.log("El resultado de la expresión: " + (a != b));
+console.log("El resultado de la expresión: " + (a > b));
+console.log("El resultado de la expresión: " + (a < b));
+console.log("El resultado de la expresión: " + (a >= b));
+console.log("El resultado de la expresión: " + (a <= b));
