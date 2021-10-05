031446010d5c5185e27fa32baccb9d9cb18d46fb
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-10-05 09:11:31 +0200

    Ampliando el javascript de estos apartados

--- Tema 2/Apartado 8, 9 y 10/script.js
+++ Tema 2/Apartado 8, 9 y 10/script.js
@@ -8,5 +8,15 @@ function devolver2() {
   texto = "Adios";
 }
 
+function multiplicar (param) {
+  console.log(param * 2)
+}
+
+function multiplicar(x, y) {
+  console.log(x * y);
+}
+
 console.log(devolver1());
 console.log(devolver2());
+console.log(multiplicar1(4));
+console.log(multiplicar(2, 3));
