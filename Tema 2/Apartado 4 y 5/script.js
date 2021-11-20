4d326fdfd72e3ff0064d4eadb3af9617f6200899
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-11-20 17:39:06 +0100

    Optimizar código

--- Tema 2/Apartado 4 y 5/script.js
+++ Tema 2/Apartado 4 y 5/script.js
@@ -1,59 +1,57 @@
-// I/O alert, console.log, confirm y prompt
-alert("Hola mundo");
+var num1;
+var num2;
 
+alert("Hola mundo");
 console.log("Otro hola mundo");
 
 let respuesta = confirm ("¿Desea cancelar la suscripción?");
 alert ("Usted ha contestado que " + respuesta);
 
-// Operadores aritméticos
 function operadoresAritmeticos(num1, num2) {
-  /* let num1 = 3;
-  let num2 = 5; */
-  console.log (num1 + " + " + num2 + " = " + (num2 += num1)); // 8
-  console.log (num1 + " - " + num2 + " = " + (num2 -= num1)); // 2
-  console.log (num1 + " * " + num2 + " = " + (num2 *= num1)); // 15
-  console.log (num1 + " / " + num2 + " = " + (num2 /= num1)); // 1
-  console.log (num1 + " % " + num2 + " = " + (num2 %= num1)); // 2
+
+  num1 = 3;
+  num2 = 5;
+
+  console.log (num1 + " + " + num2 + " = " + (num2 += num1));
+  console.log (num1 + " - " + num2 + " = " + (num2 -= num1));
+  console.log (num1 + " * " + num2 + " = " + (num2 *= num1));
+  console.log (num1 + " / " + num2 + " = " + (num2 /= num1));
+  console.log (num1 + " % " + num2 + " = " + (num2 %= num1));
 }
 
-// Asignación
-let num1 = 5;
-let num2 = 8;
+num1 = 5;
+num2 = 8;
 let resultado1;
 let resultado2;
 
-resultado1 = ((num1+num2) * 200) / 100;
+resultado1 = ((num1 + num2) * 200) / 100;
 resultado2 = resultado1 % 3;
-resultado1 = ++num1;
-resultado2 = num2++;
-resultado1 = --num1;
-resultado2 = num2--;
-resultado1 = -resultado2;
+resultado3 = ++num1;
+resultado4 = num2++;
+resultado5 = --num1;
+resultado6 = num2--;
+resultado7 = -resultado2;
 
-// Comparación
 let a = 4;
 let b = 5;
 let c = "5";
 
-console.log("El resultado de la expresión: " + (a == c));
-console.log("El resultado de la expresión: " + (a === c));
-console.log("El resultado de la expresión: " + (a != c));
-console.log("El resultado de la expresión: " + (a !== c));
-console.log("El resultado de la expresión: " + (a == b));
-console.log("El resultado de la expresión: " + (a != b));
-console.log("El resultado de la expresión: " + (a > b));
-console.log("El resultado de la expresión: " + (a < b));
-console.log("El resultado de la expresión: " + (a >= b));
-console.log("El resultado de la expresión: " + (a <= b));
-
-// Operadores lógicos
-console.log("El resultado de la expresión: " + (false && false));
-console.log("El resultado de la expresión: " + (false && true));
-console.log("El resultado de la expresión: " + (true && false));
-console.log("El resultado de la expresión: " + (true && true));
-console.log("El resultado de la expresión: " + (false || false));
-console.log("El resultado de la expresión: " + (false || true));
-console.log("El resultado de la expresión: " + (true || false));
-console.log("El resultado de la expresión: " + (true || true));
-console.log("El resultado de la expresión: " + (!false));
+console.log("Resultado: " + (a == c));
+console.log("Resultado: " + (a === c));
+console.log("Resultado: " + (a != c));
+console.log("Resultado: " + (a !== c));
+console.log("Resultado: " + (a == b));
+console.log("Resultado: " + (a != b));
+console.log("Resultado: " + (a > b));
+console.log("Resultado: " + (a < b));
+console.log("Resultado: " + (a >= b));
+console.log("Resultado: " + (a <= b));
+console.log("Resultado: " + (false && false));
+console.log("Resultado: " + (false && true));
+console.log("Resultado: " + (true && false));
+console.log("Resultado: " + (true && true));
+console.log("Resultado: " + (false || false));
+console.log("Resultado: " + (false || true));
+console.log("Resultado: " + (true || false));
+console.log("Resultado: " + (true || true));
+console.log("Resultado: " + (!false));
